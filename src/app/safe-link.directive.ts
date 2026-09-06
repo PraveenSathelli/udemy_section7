157a03158faa910d94f992bfa2315638b59a0608
Praveen Sathelli <praveensathelli11@gmail.com>
2026-09-06 11:48:14 +0530

    adding param path and typecasting event.target as HTMLAnchorElement

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -16,6 +16,8 @@ export class SafeLinkDirective {
   onConfirmLeavePage(event:MouseEvent) {
     const wantsToLeave = window.confirm('Do you want to leave the app?')
     if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href = address + '?from=myapp'
       return;
     }
    event?.preventDefault();
